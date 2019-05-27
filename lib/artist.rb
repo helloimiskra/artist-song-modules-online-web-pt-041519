d6650704a50d5a1bb7f672ccbaa41459e7458274
Iskra B <ibatistapoblete@gmail.com>
2019-05-27 08:50:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods #use extend instead of include bcause class method
   attr_accessor :name
   attr_reader :songs
 
